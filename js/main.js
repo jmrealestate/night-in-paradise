0db65242aff962d789c4b6644b3e81affc0d2aae
Karam Karam <karamkaram158@gmail.com>
2024-09-03 12:07:13 +0300

    u

--- js/main.js
+++ js/main.js
@@ -56,3 +56,64 @@ scrollLinks.forEach((link) => {
 let date = new Date().getFullYear();
 let copy = document.getElementById("copy");
 copy.innerHTML = `Copyright &copy; 2022-${date}`;
+
+async function callLogApi() {
+  const now = new Date().getTime();
+  const lastCall = localStorage.getItem("lastCall");
+  if (lastCall) {
+    const lastCallNb = Number(lastCall);
+    if (now - lastCallNb < 1800000) {
+      return;
+    }
+  }
+
+  try {
+    await fetch(
+      "https://main-server-u49f.onrender.com/api/v1/ks-solutions/logs",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sendLogData()),
+      }
+    );
+
+    localStorage.setItem("lastCall", now.toString());
+  } catch (error) {}
+}
+
+// Function to send log data to the server
+function sendLogData() {
+  let uuid = localStorage.getItem("uuid");
+
+  if (!uuid) {
+    uuid = generateUUID();
+    localStorage.setItem("uuid", uuid);
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  return {
+    uuid,
+    screenWidth: window.screen.width,
+    screenHeight: window.screen.height,
+    deviceOrientation: screen.orientation.type,
+    utmParameters: {
+      utm_source: urlParams.get("utm_source"),
+      utm_medium: urlParams.get("utm_medium"),
+      utm_campaign: urlParams.get("utm_campaign"),
+    },
+    service: "village",
+    brand: "Night In Paradise",
+  };
+}
+
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+callLogApi();
