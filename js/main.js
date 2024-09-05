9e45bf9fe5f4f3e710af0929d5e07514fe62840d
Karam Karam <karamkaram158@gmail.com>
2024-09-05 16:14:37 +0300

    s

--- js/main.js
+++ js/main.js
@@ -103,6 +103,7 @@ function sendLogData() {
       utm_medium: urlParams.get("utm_medium"),
       utm_campaign: urlParams.get("utm_campaign"),
     },
+
     service: "external website",
     brand: "Night In Paradise",
   };
